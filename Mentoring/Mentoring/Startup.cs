738893dd170665f1cef7d4a3a2d5ddd252ac32f5
chkaff <yutyomchik@gmail.com>
2020-11-06 18:51:42 +0500

    Update project

--- Mentoring/Mentoring/Startup.cs
+++ Mentoring/Mentoring/Startup.cs
@@ -12,25 +12,54 @@ namespace Mentoring
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private string nameData = "";
         public void ConfigureServices(IServiceCollection services)
         {
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            
             app.UseRouting();
-
+            
+            app.Map("/secret", Secret);
+            
+            app.MapWhen(context => 
+            {
+                if (context.Request.Query.ContainsKey("name"))
+                {
+                    nameData = context.Request.Query["name"];
+                    return true;
+                }
+                return false; 
+            }, HelloName);
+            
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+        
+        private  void HelloName(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync($"Hello {nameData}");
+            });
+        }
+
+        private static void Secret(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Number 42");
             });
         }
     }
